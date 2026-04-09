e3ee02781cd45f945d698642eb5fbb6178ab6ae6
Alusc <lucasknoppamaral@protonmail.com>
2026-04-08 22:18:23 -0300

    :sparkles: adiciona root

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-import './style.css'
-import { setupCounter } from './counter.js'
-import { setupRootApp } from './setupRootApp.js'
+import './style.css';
+import { setupRootApp } from './setupRootApp.js';
 
-document.querySelector('#app').innerHTML = setupRootApp();
+document.querySelector('#app').append(setupRootApp());
 
-
-
-setupCounter(document.querySelector('#counter'))
