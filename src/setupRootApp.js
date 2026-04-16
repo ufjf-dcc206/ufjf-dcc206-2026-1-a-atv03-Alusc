558e49c05d71b73febad7e5a64b47b94fe20201f
Alusc <lucasknoppamaral@protonmail.com>
2026-04-15 21:43:53 -0300

    Cria form

--- src/setupRootApp.js
+++ src/setupRootApp.js
@@ -3,6 +3,7 @@ import { getLista } from "./lista";
 export function setupRootApp(){ 
   const divRoot = document.createElement("div");
   divRoot.append(criaTitulo());
+  divRoot.append(criaForm());
   divRoot.append(criaLista());
   return divRoot;
 }
@@ -11,6 +12,34 @@ function criaTitulo(){
   const h1 = document.createElement("h1");
   h1.textContent = "App de lista";
   return h1;
+}
+
+function criaForm(){
+  const divForm = document.createElement("div");
+  const form = document.createElement("form");
+  form.name = "entry";
+  divForm.append(form);
+  
+  const input = document.createElement("input");
+  input.name = "text";
+  form.append(input);
+  
+  const button = document.createElement("button");
+  button.textContent = "Adicionar";
+  form.append(button);
+
+  setupFormSubmit(form);
+
+  return divForm;
+}
+
+function setupFormSubmit(form) {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(document.entry.text.value);
+    document.entry.text.value = "";
+    document.entry.text.focus();
+  })
 }
 
 function criaLista(){
